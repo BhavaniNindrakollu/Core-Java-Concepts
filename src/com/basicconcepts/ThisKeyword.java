0acacd436e6feea41922342f026f18169e7ec0f6
BhavaniNindrakollu <durgabhavaninindrakollu9@gmail.com>
2026-06-15 20:03:15 +0530

    this keyword example

---
+++ src/com/basicconcepts/ThisKeyword.java
@@ -0,0 +1,24 @@
+package com.basicconcepts;
+
+public class ThisKeyword {
+	
+	int salary;
+	ThisKeyword(int salary){
+		
+		this.salary=salary;
+		
+	}
+	void display() {
+		System.out.println("Salary is:"+salary);
+	}
+
+	public static void main(String[] args) {
+		
+		ThisKeyword tk= new ThisKeyword(50000);
+		tk.display();
+		
+		
+
+	}
+
+}
