e39f8000d50482d2a5a746f3a765580d44002011
BhavaniNindrakollu <durgabhavaninindrakollu9@gmail.com>
2026-07-08 07:47:20 +0530

    Added Java Multiple Inheritance example using interfaces and resolved the Diamond Problem

---
+++ src/com/oppsconcept/MultipleInheritance.java
@@ -0,0 +1,45 @@
+package com.oppsconcept;
+
+//Demonstrates Multiple Inheritance using Interfaces.
+//Java does not support multiple inheritance through classes.
+//A class cannot extend more than one class.
+//Multiple inheritance through classes can lead to the Diamond Problem.
+
+interface Camera{
+	
+	default void start () {
+		System.out.println("Camera is turning on....");
+		
+		
+	}
+}
+
+interface MediaPlayer{
+	
+	default void start () {
+		System.out.println("MediaPlayer is starting....");
+		
+		
+	}
+}
+
+class SmartPhone implements Camera ,MediaPlayer{
+	public void start() {
+		Camera.super.start();
+		MediaPlayer.super.start();
+		
+	}
+}
+
+public class MultipleInheritance {
+	
+	
+	public static void main(String[] args) {
+		SmartPhone sm = new SmartPhone();
+		sm.start();
+		
+
+
+	}
+
+}
